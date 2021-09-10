15624ddd8a2ac95ef6c4bc74d3ebe1d7869c7936
FilipKoniuszewski <82803009+FilipKoniuszewski@users.noreply.github.com>
2021-09-11 00:38:29 +0200

    Add navbar

--- server.py
+++ server.py
@@ -1,1 +1,13 @@
-# put your code here
+from flask import Flask, render_template, redirect, request
+import connection
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
