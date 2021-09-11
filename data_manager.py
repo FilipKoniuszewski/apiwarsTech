0ea47ce67fa5f43d10d0dccf75d96dc19e6a5504
FilipKoniuszewski <82803009+FilipKoniuszewski@users.noreply.github.com>
2021-09-11 02:29:22 +0200

    add registration and login as requested

---
+++ data_manager.py
@@ -0,0 +1,43 @@
+import connection
+
+
+@connection.connection_handler
+def check_if_user_in_database(cursor, email):
+    query = f""" SELECT * 
+                FROM users 
+                WHERE email = '{email}'
+
+            """
+    cursor.execute(query)
+    result = cursor.fetchone()
+    return result
+
+
+@connection.connection_handler
+def find_user_id_by_email(cursor, email):
+    query = f"""SELECT id
+                FROM users
+                WHERE email = '{email}'
+            """
+    cursor.execute(query)
+    return cursor.fetchone()
+
+
+@connection.connection_handler
+def save_user(cursor, email, password):
+    query = f"""INSERT INTO users(email,password)
+                VALUES  ('{email}', '{password}')  
+
+            """
+    return cursor.execute(query)
+
+
+@connection.connection_handler
+def get_password_by_email(cursor, email):
+    query = f"""SELECT password
+                FROM users
+                WHERE email = '{email}' 
+            """
+    cursor.execute(query)
+    result = cursor.fetchone()
+    return result
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 import connection
+import data_manager,util
 
 app = Flask(__name__)
 
@@ -7,6 +8,38 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template("index.html")
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    # if request.method == 'POST':
+    #     if not data_manager.check_if_user_in_database(request.form['email']):
+    #         email = request.form['email']
+    #         password = util.hash_password(request.form['password'])
+    #         data_manager.save_user(email, password)
+    #         return redirect('/')
+    #     else:
+    #         return render_template('register.html', info="Sorry but that mail is already in use")
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if data_manager.check_if_user_in_database(request.form['email']):
+            password = request.form['password']
+            saved_password = data_manager.get_password_by_email(request.form['email'])['password']
+            if util.verify_password(password, saved_password):
+                user = request.form['email']
+                user_id = data_manager.find_user_id_by_email(user)
+                session['id'] = user_id['id']
+                session['user'] = user
+                return redirect('/')
+            else:
+                return render_template('login.html', info="Your login or password is incorrect")
+        else:
+            return render_template('login.html', info="user with such e-mail does not exist")
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
